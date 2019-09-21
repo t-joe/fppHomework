df25a33b09abbeab77870abfff4efc1373d2e647
t-joe <tuugii.soft@gmail.com>
2019-09-21 18:21:40 -0500

    homework10

--- Homework/src/homework10/Main.java
+++ Homework/src/homework10/Main.java
@@ -3,8 +3,22 @@ package homework10;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyBST bt = new MyBST();
+		int[] arr = { 13, 3, 4, 12, 14, 10, 5, 1, 8, 2, 7, 9, 11, 6, 18, 17, 20, 19, 21, 0 };
+		for (int i : arr)
+			bt.add(i);
+		System.out.println("size: " + bt.size());
+		System.out.println("InOrder:");
+		bt.inOrder();
+		System.out.println("\nPreOrder: ");
+		bt.preOrder();
+		System.out.println("\nPostOrder: ");
+		bt.postOrder();
+		System.out.println("\nContains 17?: " + bt.contains(17));
+		System.out.println("Contains 30?: " + bt.contains(30));
+		System.out.println("Min value: " + bt.findMin());
+		System.out.println("Max value: " + bt.findMax());
+		System.out.println("Leaf nodes: " + bt.leafNodes());
 	}
 
 }
--- Homework/src/homework10/MyBST.java
+++ Homework/src/homework10/MyBST.java
@@ -1,20 +1,22 @@
 package homework10;
 
 public class MyBST {
-	Node root;
+	private Node root;
+	private int size = 0;
 
 	public MyBST() {
 		this.root = null;
+		size = 0;
 	}
 
 	public void preOrder() {
-		preOrder(root);		
+		preOrder(root);
 	}
-	private void preOrder(Node n)
-	{
-		if(n == null)
+
+	private void preOrder(Node n) {
+		if (n == null)
 			return;
-		System.out.println(n.getData());
+		System.out.printf("%3s", n.getData());
 		preOrder(n.getLeft());
 		preOrder(n.getRight());
 	}
@@ -22,51 +24,52 @@ public class MyBST {
 	public void inOrder() {
 		inOrder(root);
 	}
-	public void inOrder(Node n)
-	{
-		if(n == null)
+
+	public void inOrder(Node n) {
+		if (n == null)
 			return;
 		inOrder(n.getLeft());
-		System.out.println(n.getData());
+		System.out.printf("%3s", n.getData());
 		inOrder(n.getRight());
 	}
 
 	public void postOrder() {
 		postOrder(root);
 	}
-	public void postOrder(Node n)
-	{
-		if(n== null)
+
+	public void postOrder(Node n) {
+		if (n == null)
 			return;
 		postOrder(n.getLeft());
 		postOrder(n.getRight());
-		System.out.println(n.getData());
+		System.out.printf("%3s", n.getData());
 	}
 
 	public boolean contains(int value) {
 		return contains(root, value);
 	}
-	private boolean contains(Node n, int value)
-	{
-		if(n == null)return false;
-		if(n.getData() == value)return true;
-		if(n.getData() <= value)
-		{
-			if(n.getRight() != null)
+
+	private boolean contains(Node n, int value) {
+		if (n == null)
+			return false;
+		if (n.getData() == value)
+			return true;
+		if (n.getData() <= value) {
+			if (n.getRight() != null)
 				return contains(n.getRight(), value);
-			else return false;
-		}
-		else
-		{
-			if(n.getLeft() != null)
+			else
+				return false;
+		} else {
+			if (n.getLeft() != null)
 				return contains(n.getLeft(), value);
-			else return false;
+			else
+				return false;
 		}
-		
+
 	}
 
 	public int size() {
-		return 0;
+		return size;
 	}
 
 	public Node getRoot() {
@@ -75,59 +78,74 @@ public class MyBST {
 
 	public int findMin() {
 		Node n = findMin(root);
-		if(n == null ) return -1;
+		if (n == null)
+			return -1;
 		return n.getData();
 	}
-	private Node findMin(Node n)
-	{
-		if(n == null) return null ;
-		if(n.getLeft() == null)
+
+	private Node findMin(Node n) {
+		if (n == null)
+			return null;
+		if (n.getLeft() == null)
 			return n;
 		return findMin(n.getLeft());
 	}
 
 	public int findMax() {
 		Node n = findMax(root);
-		if(n == null ) return -1;
+		if (n == null)
+			return -1;
 		return n.getData();
 	}
-	private Node findMax(Node n)
-	{
-		if(n == null) return null ;
-		if(n.getRight() == null)
+
+	private Node findMax(Node n) {
+		if (n == null)
+			return null;
+		if (n.getRight() == null)
 			return n;
-		return findMin(n.getRight());
+		return findMax(n.getRight());
 	}
 
 	public int leafNodes() {
-		return 0;
+		int leafNodes = 0;
+		leafNodes = leafNodes(root);
+		return leafNodes;
+	}
+	private int leafNodes(Node n)
+	{
+		int i = 0;
+		if(n == null) return i;
+		if(n.getLeft() == null && n.getRight() == null) i++;
+		i += leafNodes(n.getLeft());
+		i += leafNodes(n.getRight());
+		return i;
 	}
 
 	public void add(int data) {
 		Node newNode = new Node(data);
+		boolean result = false;
 		if (root == null) {
 			root = newNode;
 			return;
 		}
-		add(root, newNode);
+		result = add(this.root, newNode);
+		if(result)
+			size++;
 	}
 
-	private boolean add(Node root, Node node) {
-		if (root.getData() <= node.getData()) {
-			if (root.getRight() != null)
-				return add(root.getRight(), node);
-			else
-			{
-				root.setRight(node);
+	private boolean add(Node rNode, Node node) {
+		if (rNode.getData() <= node.getData()) {
+			if (rNode.getRight() != null)
+				return add(rNode.getRight(), node);
+			else {
+				rNode.setRight(node);
 				return true;
 			}
-		} else
-		{
-			if(root.getLeft() != null)
-			return add(root.getLeft(), node);
-			else
-			{
-				root.setLeft(root);
+		} else {
+			if (rNode.getLeft() != null)
+				return add(rNode.getLeft(), node);
+			else {
+				rNode.setLeft(node);
 				return true;
 			}
 		}
